3087871a56658af6c593c15cdbf33b08133bef06
holy_nova <holy_nova@126.com>
2025-03-27 23:59:47 +0800

    feat: 添加 framer-motion 依赖，优化动画效果；更新路由配置以支持名称属性

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,13 @@
+import React from "react";
 import { RouteObject } from "react-router-dom";
+
+interface RouteConfig {
+  name?: string;
+  path?: string;
+  element?: React.ReactNode;
+  children?: RouteConfig[];
+}
+
 import CombinationPage from "./pages/CombinationPage/CombinationPage";
 import DemoPage from "./pages/DemoPage/DemoPage";
 import HomePage from "./pages/HomePage/HomePage";
@@ -6,24 +15,29 @@ import MainPage from "./pages/MainPage/MainPage";
 import JsonFormatPage from "./pages/JsonFormatPage/JsonFormatPage";
 import TimerPage from "./pages/TimerPage/TimerPage";
 
-const routes: RouteObject[] = [
+const routes = [
   {
+    name: "排列组合",
     path: "/combination",
     element: <CombinationPage />,
   },
+  // {
+
+  //   path: "/demo",
+  //   element: <DemoPage />,
+  // },
   {
-    path: "/demo",
-    element: <DemoPage />,
-  },
-  {
+    name: "JavaScript to TypeScript",
     path: "/ts",
     element: <HomePage />,
   },
   {
+    name: "json format",
     path: "/json",
     element: <JsonFormatPage />,
   },
   {
+    name: "火锅定时器Timer",
     path: "/timer",
     element: <TimerPage />,
   },
@@ -32,4 +46,4 @@ const routes: RouteObject[] = [
     element: <MainPage />,
   },
 ];
-export default routes;
+export default routes as RouteConfig[];
