9da6ac9fa7f0347f8412621612d4e161f10f9f6b
holy_nova <holy_nova@126.com>
2026-06-30 09:43:58 +0800

    chore: replace old domain xiaosang.net with holynova.github.io

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -40,7 +40,7 @@ export const Header: React.FC<HeaderProps> = ({ currentKey }) => {
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              <span>xiaosang.net</span>
+              <span>holynova.github.io</span>
             </a>
           </div>
 
